04912a6b76f773d6a387a269dbd32137ecb16b22
Vinicius de Oliveira <89990797+ViniSartori@users.noreply.github.com>
2024-07-11 14:14:43 -0300

    Create CPF.py

---
+++ cpf.py
@@ -0,0 +1,29 @@
+cpf = input('Digite seu CPF: ')
+nove_digitos = cpf[:9]
+contador_regressivo_1 = 10
+contador_regressivo_2 = 11
+
+resultado_digito_1 = 0
+for digito in nove_digitos:
+    resultado_digito_1 += int(digito) * contador_regressivo_1
+    contador_regressivo_1 -= 1
+
+primeiro_digito = (resultado_digito_1 * 10) % 11
+
+primeiro_digito = primeiro_digito if primeiro_digito <= 9 else 0
+
+dez_digitos = str(nove_digitos) + str(primeiro_digito)
+
+resultado_digito_2 = 0
+for digito_2 in dez_digitos:
+    resultado_digito_2 += int(digito_2) * contador_regressivo_2
+    contador_regressivo_2 -= 1
+
+segundo_digito = (resultado_digito_2 * 10) % 11
+
+cpf_calculado = str(dez_digitos) + str(segundo_digito)
+
+if cpf_calculado == cpf:
+    print('CPF validado')
+else:
+    print('Você digitou um CPF inválido')
