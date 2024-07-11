be75e194da874f0dc8d2e58a0afdab954fe51696
Vinicius de Oliveira <89990797+ViniSartori@users.noreply.github.com>
2024-07-11 15:12:55 -0300

    Update cpf.py

--- cpf.py
+++ cpf.py
@@ -1,7 +1,14 @@
-cpf = input('Digite seu CPF: ')
+import sys
+
+cpf = input('Digite seu CPF: ').replace('.', '').replace('-', '')
+
+sequencial = cpf == cpf[0] * len(cpf)
+if sequencial:
+    print('Você enviou dados sequenciais')
+    sys.exit()
+
 nove_digitos = cpf[:9]
 contador_regressivo_1 = 10
-contador_regressivo_2 = 11
 
 resultado_digito_1 = 0
 for digito in nove_digitos:
@@ -9,10 +16,10 @@ for digito in nove_digitos:
     contador_regressivo_1 -= 1
 
 primeiro_digito = (resultado_digito_1 * 10) % 11
-
 primeiro_digito = primeiro_digito if primeiro_digito <= 9 else 0
 
 dez_digitos = str(nove_digitos) + str(primeiro_digito)
+contador_regressivo_2 = 11
 
 resultado_digito_2 = 0
 for digito_2 in dez_digitos:
@@ -20,6 +27,7 @@ for digito_2 in dez_digitos:
     contador_regressivo_2 -= 1
 
 segundo_digito = (resultado_digito_2 * 10) % 11
+segundo_digito = segundo_digito if segundo_digito <= 9 else 0
 
 cpf_calculado = str(dez_digitos) + str(segundo_digito)
 
